a68ae4fc811d253e2ec8b953aeec016dfe658c77
Train0303 <rjsdnxogh55@gmail.com>
2023-10-31 14:05:27 +0900

    Conf: First Configuration

---
+++ greengift/src/main/java/com/example/greengift/GreengiftApplication.java
@@ -0,0 +1,13 @@
+package com.example.greengift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreengiftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreengiftApplication.class, args);
+    }
+
+}
---
+++ greengift/src/test/java/com/example/greengift/GreengiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greengift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreengiftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
